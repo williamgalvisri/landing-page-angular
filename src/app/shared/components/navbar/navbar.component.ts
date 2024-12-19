515ebc846cd5d15a044f2514d8756206386dc285
William Galvis <williamsoluntech@gmail.com>
2024-12-18 21:29:29 -0500

    update navbar with a toogle button to show and hiden options menu
    add media queries to improve user experience
    
    add new assets svg icons

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+    public isOpen: boolean = false;
 }
