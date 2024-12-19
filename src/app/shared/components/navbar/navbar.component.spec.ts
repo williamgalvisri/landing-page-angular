cb42a0dd75526ec20d1949e33e497b11cc4fca06
William Galvis <williamsoluntech@gmail.com>
2024-12-19 11:48:06 -0500

    add navbar tests to the input test suite
    add context menu
    
    add some animation to the context menu

--- src/app/shared/components/navbar/navbar.component.spec.ts
+++ src/app/shared/components/navbar/navbar.component.spec.ts
@@ -3,21 +3,48 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+    let component: NavbarComponent;
+    let fixture: ComponentFixture<NavbarComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [NavbarComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [NavbarComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(NavbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(NavbarComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should have isOpen set to false', () => {
+        expect(component.isOpen).toBeFalse();
+    });
+
+
+    it('should toggle isOpen property', () => {
+        component.toggle();
+        expect(component.isOpen).toBeTrue();
+        component.toggle();
+        expect(component.isOpen).toBeFalse();
+    });
+
+    it('should be exist the element when isOpen is true', () => {
+        component.isOpen = true;
+        fixture.detectChanges();
+        const wrapperMenuMobile = fixture.nativeElement.querySelector('.wrapper-menu-mobile__context');
+        expect(wrapperMenuMobile).toBeTruthy();
+    });
+
+
+    it('should not exist the element when isOpen is false', () => {
+        component.isOpen = false;
+        fixture.detectChanges();
+        const wrapperMenuMobile = fixture.nativeElement.querySelector('.wrapper-menu-mobile__context');
+        expect(wrapperMenuMobile).toBeFalsy();
+    });
+
 });
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 import { CommonModule } from '@angular/common';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
-  selector: 'app-navbar',
-  imports: [ButtonComponent, CommonModule],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+    selector: 'app-navbar',
+    imports: [
+        ButtonComponent,
+        CommonModule
+    ],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.css',
+    animations: [
+        trigger(
+            'showContext',
+            [
+                // Add animation here
+                transition(':enter', [
+                    style({ opacity: 0 }),
+                    animate('0.3s', style({ opacity: 1 })),
+                ]),
+                transition(':leave', [
+                    style({ opacity: 1 }),
+                    animate('0.3s', style({ opacity: 0 })),
+                ]),
+            ]
+        )
+    ],
 })
 export class NavbarComponent {
     public isOpen: boolean = false;
+
+    public toggle(): void {
+        this.isOpen = !this.isOpen;
+    }
 }
