ad72a161175d826728bad72fa2ba2e901d1b23b8
William Galvis <williamsoluntech@gmail.com>
2024-12-26 16:17:21 -0500

    add style to case studies section

--- src/app/pages/home/components/case-studies/case-studies.component.ts
+++ src/app/pages/home/components/case-studies/case-studies.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TitleSectionComponent } from "../../../../shared/components/title-section/title-section.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-case-studies',
-  imports: [],
+  imports: [TitleSectionComponent, CommonModule],
   templateUrl: './case-studies.component.html',
   styleUrl: './case-studies.component.css'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { SharedModule } from '@shared/shared.module';
 import { CtaComponent } from './components/cta/cta.component';
 import { ServicesComponent } from "./components/services/services.component";
 import { MiddleCtaComponent } from "./components/middle-cta/middle-cta.component";
+import { CaseStudiesComponent } from "./components/case-studies/case-studies.component";
 
 @Component({
   selector: 'app-home',
-  imports: [SharedModule, CtaComponent, ServicesComponent, MiddleCtaComponent],
+  imports: [SharedModule, CtaComponent, ServicesComponent, MiddleCtaComponent, CaseStudiesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
